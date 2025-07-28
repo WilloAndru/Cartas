cf10fced3c6e81bd8d6af4e0e0b5cfc5d682f590
Willoandru <wilsonandrescriollo@gmail.com>
2025-07-28 17:57:22 -0500

    Basename /Cartas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ContextProvider } from './context/Context';
-import Principal from './pages/Principal';
-import Mazo from './pages/Mazo';
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ContextProvider } from "./context/Context";
+import Principal from "./pages/Principal";
+import Mazo from "./pages/Mazo";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Mazo />,
+    },
+    {
+      path: "/1",
+      element: <Principal />,
+    },
+  ],
   {
-    path: '/',
-    element: <Mazo />,
-  },
-  {
-    path: '/1',
-    element: <Principal />,
-  },
-]);
+    basename: "/Cartas",
+  }
+);
 
 function App() {
   return (
@@ -25,4 +30,3 @@ function App() {
 }
 
 export default App;
-
